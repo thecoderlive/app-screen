1e0d816d44ebde2e95bf478dec49517ee3e8a814
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-08-11 11:02:33 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "8afd9125-40f2-49ca-88ce-7c12ec00e03e",
+    "home_feed": [
+        {
+            "id": "2eb88dd0-9368-46d3-8786-1945f8ec21be",
+            "artist_name": "doctor",
+            "home_carousel": [
+                {
+                    "id": "55134f2a-bb0c-4a6c-926e-732312ac6566",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/368.png"
+                },
+                {
+                    "id": "87bb29c7-aa4d-42e5-9587-5c7614b6b82e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/40.png"
+                },
+                {
+                    "id": "821a1a7e-5bf8-4710-9d9a-e2d09f408275",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/104.png"
+                },
+                {
+                    "id": "7c652a3e-d50f-4860-85f3-6bc188d94a11",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/226.png"
+                },
+                {
+                    "id": "5dadb697-f409-46ff-8526-5b65b5e6f379",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/334.png"
+                }
+            ],
+            "art_caption": "fight",
+            "art_description": "Contain consumer population phone."
+        },
+        {
+            "id": "39ee81e1-c2b2-4947-b357-03ba33e9cbcf",
+            "artist_name": "skin",
+            "home_carousel": [
+                {
+                    "id": "85022a58-bf38-41b6-b506-b20b591f8fee",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/12.png"
+                },
+                {
+                    "id": "115fb3b1-c681-465e-840c-0c3307cc451c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png"
+                },
+                {
+                    "id": "78424486-086f-42e1-8c96-64e0f6284e61",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/273.png"
+                },
+                {
+                    "id": "addc7596-397e-4aa5-8d0a-e3d1eef3e6e6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/246.png"
+                },
+                {
+                    "id": "092c4446-59d1-48fa-8a31-89ceb93e55e5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/10.png"
+                }
+            ],
+            "art_caption": "for",
+            "art_description": "Administration car she door not. Sit series yard staff. Present consider meeting important car create development respond. Statement from must anything line energy wide line."
+        },
+        {
+            "id": "8e8f2db9-018d-483d-a773-2828f6c4ba8d",
+            "artist_name": "skill",
+            "home_carousel": [
+                {
+                    "id": "bb6f6ba5-447d-4379-a2ef-b55f3cf9b060",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/365.png"
+                },
+                {
+                    "id": "fb21e11e-e5ed-4549-ab63-609124aa1b38",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/365.png"
+                },
+                {
+                    "id": "60350fa0-9730-4f22-aa49-7f140fabcb0b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                },
+                {
+                    "id": "a7b10593-fd37-4696-a29d-357211501377",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/334.png"
+                },
+                {
+                    "id": "65a651e7-361f-401c-bf06-85b63ceb3a82",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/252.png"
+                }
+            ],
+            "art_caption": "begin",
+            "art_description": "Condition government recognize produce to stop far."
+        },
+        {
+            "id": "0ca824d6-db2e-4754-8ba1-746255e10304",
+            "artist_name": "defense",
+            "home_carousel": [
+                {
+                    "id": "24a5f88f-e418-4b00-9e08-dc30a85224ff",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/155.png"
+                },
+                {
+                    "id": "927b8a91-3178-4743-911e-b563e7fabeb0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+                },
+                {
+                    "id": "ba6fbd53-0403-4b53-96fc-8a7481018bdb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/127.png"
+                },
+                {
+                    "id": "fff70e69-aa25-469b-aa65-948c3550725c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/354.png"
+                },
+                {
+                    "id": "84a74ce7-fe87-41e9-89b1-851a072d434e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/126.png"
+                }
+            ],
+            "art_caption": "resource",
+            "art_description": "Shake manager president pattern stock choice it participant. She process car smile. Nothing cold other official."
+        },
+        {
+            "id": "38bf40ee-1f4f-405e-85e2-773a08df7434",
+            "artist_name": "movie",
+            "home_carousel": [
+                {
+                    "id": "43aa5235-934a-45c6-93e7-bfb7249fbad6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/336.png"
+                },
+                {
+                    "id": "6bc37e25-0c4e-4494-88ed-b024c802e962",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/327.png"
+                },
+                {
+                    "id": "9a7ce077-70a0-4050-b02f-02e872059871",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/208.png"
+                },
+                {
+                    "id": "79605a02-f4e7-49f4-bf1b-db7a3fbc5688",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                },
+                {
+                    "id": "845a47cb-5866-4b0f-9df0-7b837f32ce71",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/270.png"
+                }
+            ],
+            "art_caption": "most",
+            "art_description": "Expect anything go. State high plan agree. Year accept cup house laugh eat tree prove."
+        }
+    ]
+}
